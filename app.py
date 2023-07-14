ffc046e24b8ab7899f9f96107644a45fbc6bbc27
RuslanKudarov <139555609+RuslanKudarov@users.noreply.github.com>
2023-07-14 19:17:34 +0300

    app.py

--- app.py
+++ app.py
@@ -86,8 +86,8 @@ def run():
                              index = 0)
         SrBallDokObr = st.number_input('Ср. балл док-та об образовании',
                                        min_value = 3, max_value = 5,
-                                       value = 3, step = .01,
-                                       help = 'с точностью до 2-х знаков после запятой')
+                                       value = 3, step = 0.01,
+                                       help = "с точностью до 2-х знаков после запятой")
         EgeMath = st.slider('Математика ЕГЭ', min_value = 36, max_value = 100,
                             value = 36, step = 1)
         EgeRus = st.slider('Русский язык ЕГЭ', min_value = 36, max_value = 100,
